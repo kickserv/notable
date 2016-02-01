2b484219921074d50398fc4aada578295e7db585
Joshua Coffee <josh@joffeedesign.com>
2016-02-01 12:15:34 -0500

    Revert "add notable package"
    This reverts commit 065b80bc9f81f048271105a2f89863ef4a08087e.

--- Godeps/_workspace/src/github.com/joshualcoffee/notable/note.go
+++
@@ -1,8 +0,0 @@
-package notable
-
-type Note struct {
-	Author    string `json:"author"`
-	AvatarURL string `json:"avatar_url"`
-	Category  string `json:"category"`
-	Text      string `json:"text"`
-}
